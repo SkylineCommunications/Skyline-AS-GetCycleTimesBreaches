613cb7ad96603a18c59c57b046e05c0308179b0c
André Camões <andre.camoes@skyline.be>
2023-05-29 11:58:19 +0100

    First Commit

---
+++ GetSquadBreaches_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GetSquadBreaches_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("GetSquadBreaches_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A09947F2-D389-41EF-A60D-C2947D1972C9")]
+[assembly: AssemblyVersion("1.0.0.0")]
